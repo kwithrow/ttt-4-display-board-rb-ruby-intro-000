478449a1bc465b87c6a692157db087a7e90cf55a
bold-class-9124 <kirstiewithrow@gmail.com>
2021-02-22 03:04:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
-board = [" ", "X", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
+# Define display_board that accepts a board and prints
+# out the current state.
+def display_board
   puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
 end
-
-display_board(board)
+display_board
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
